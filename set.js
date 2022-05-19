ee8291da22053e14823e40406704af79a926c519
catchchances <catchchances@outlook.com>
2022-05-19 17:17:04 +0800

    设置默认浏览器为chrome

---
+++ set.js
@@ -0,0 +1,2 @@
+var wshShell = new ActiveXObject("WScript.Shell");
+wshShell.Run('%SystemRoot%\\system32\\WindowsPowerShell\\v1.0\\powershell.exe -ExecutionPolicy Bypass -File "D:\\apps\\shell\\setDefaultBrowser-chrome.ps1"', 0, false);
